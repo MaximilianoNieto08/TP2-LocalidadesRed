f9388fb11be776b6ae93fe9de1468caca9084f4b
maximiliano nieto <maximiliano.n.nieto@gmail.com>
2026-05-05 21:13:56 -0300

    clase Sistema

--- src/modelo/Sistema.java
+++ src/modelo/Sistema.java
@@ -2,22 +2,35 @@ package modelo;
 
 import java.util.List;
 
+
 public class Sistema {
 
 	private Grafo grafo = new Grafo();
 	
-	public void ingresarLocalidad() {
+	public void ingresarLocalidad(String nombre, String provincia, double latitud, double longitud) {
+		
+		Localidad loc= new Localidad(nombre, provincia, latitud, longitud);
+		this.grafo.agregarLocalidad(loc);
 		
 	}
 	
-	public List<Conexion> calcularMST() {
+	public List<Conexion> calcularCosto(double costoPorKm, double porcentajeExtra, double costoInterprovincial){
+		CalculadorCosto calculo = new CalculadorCosto(costoPorKm, porcentajeExtra, costoInterprovincial);
+        grafo.generarGrafoCompleto(calculo);
+        
+		List <Conexion> mst = this.calcularMST();
+		
+		return mst;
+		
+		
+		
+	}
+	private List<Conexion> calcularMST() {
 		KruskalMST kruskal = new KruskalMST();
         List<Conexion> mst = kruskal.calcularMST(grafo.getLocalidades(), grafo.getConexiones());
         return mst;
 	}
-	public double calcularCosto() {
-		return 0.0;
-	}
+	
 	
 	
 	
